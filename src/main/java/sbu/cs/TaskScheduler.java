78a88f6e4a782856c1cf12e6755a68f557b1baef
Fateme Lashkari <fatemelashkariii84@gamil.com>
2024-05-10 11:40:59 +0330

    sorting the tasks array list

--- src/main/java/sbu/cs/TaskScheduler.java
+++ src/main/java/sbu/cs/TaskScheduler.java
@@ -1,6 +1,8 @@
 package sbu.cs;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class TaskScheduler
@@ -9,7 +11,7 @@ public class TaskScheduler
     {
         /*
             ------------------------- You don't need to modify this part of the code -------------------------
-         */
+        */
         String taskName;
         int processingTime;
 
@@ -17,12 +19,16 @@ public class TaskScheduler
             this.taskName = taskName;
             this.processingTime = processingTime;
         }
+        public int getProcessingTime() {
+            return processingTime;
+        }
         /*
             ------------------------- You don't need to modify this part of the code -------------------------
-         */
+        */
 
         @Override
         public void run() {
+
             /*
             TODO
                 Simulate utilizing CPU by sleeping the thread for the specified processingTime
@@ -33,7 +39,7 @@ public class TaskScheduler
     public static ArrayList<String> doTasks(ArrayList<Task> tasks)
     {
         ArrayList<String> finishedTasks = new ArrayList<>();
-
+        Collections.sort(tasks , Comparator.comparing(Task::getProcessingTime)); // for sorting the objects base on their processing time
         /*
         TODO
             Create a thread for each given task, And then start them based on which task has the highest priority
@@ -41,11 +47,24 @@ public class TaskScheduler
             You have to wait for each task to get done and then start the next task.
             Don't forget to add each task's name to the finishedTasks after it's completely finished.
          */
-
+        for (int i = tasks.size()-1 ; i >= 0 ; i--) { // coping the tasks array list in the finishedTasks array list
+            finishedTasks.add(tasks.get(i).taskName);
+        }
         return finishedTasks;
     }
 
     public static void main(String[] args) {
         // Test your code here
+        ArrayList<Task> tasks = new ArrayList<>();
+
+        tasks.add(new Task("A", 100));
+        tasks.add(new Task("B", 150));
+        tasks.add(new Task("C", 200));
+        tasks.add(new Task("E", 130));
+        tasks.add(new Task("F", 300));
+
+     //   for (int i = 0 ; i < tasks.size() ; i++ ) {
+            System.out.println(doTasks(tasks));
+       // }
     }
 }

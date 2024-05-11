c44c9ad21e6dc1fd296e9d32adabe11f9d24bf0b
Fateme Lashkari <fatemelashkariii84@gamil.com>
2024-05-11 21:38:42 +0330

    tell the threads to do their tasks and wait for the next thread base on the processing time then run their tasks

--- src/main/java/sbu/cs/TaskScheduler.java
+++ src/main/java/sbu/cs/TaskScheduler.java
@@ -1,5 +1,7 @@
 package sbu.cs;
 
+import com.sun.source.tree.TryTree;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -33,11 +35,17 @@ public class TaskScheduler
             TODO
                 Simulate utilizing CPU by sleeping the thread for the specified processingTime
              */
+
+            try{
+                System.out.println(taskName + " is started ");
+                Thread.sleep(processingTime);
+            }catch(Exception e) {
+                System.out.println(e.getMessage());
+            }
         }
     }
 
-    public static ArrayList<String> doTasks(ArrayList<Task> tasks)
-    {
+    public static ArrayList<String> doTasks(ArrayList<Task> tasks) throws InterruptedException {
         ArrayList<String> finishedTasks = new ArrayList<>();
         Collections.sort(tasks , Comparator.comparing(Task::getProcessingTime)); // for sorting the objects base on their processing time
         /*
@@ -47,21 +55,31 @@ public class TaskScheduler
             You have to wait for each task to get done and then start the next task.
             Don't forget to add each task's name to the finishedTasks after it's completely finished.
          */
+        Thread thread;
+        for (int i = 0 ; i < tasks.size() ; i++) {
+            thread = new Thread(tasks.get(i));
+            thread.start();
+            try {
+                thread.join();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
         for (int i = tasks.size()-1 ; i >= 0 ; i--) { // coping the tasks array list in the finishedTasks array list
             finishedTasks.add(tasks.get(i).taskName);
         }
         return finishedTasks;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         // Test your code here
         ArrayList<Task> tasks = new ArrayList<>();
 
-        tasks.add(new Task("A", 100));
-        tasks.add(new Task("B", 150));
-        tasks.add(new Task("C", 200));
-        tasks.add(new Task("E", 130));
-        tasks.add(new Task("F", 300));
+        tasks.add(new Task("A", 1000));
+        tasks.add(new Task("B", 1200));
+        tasks.add(new Task("C", 5000));
+        tasks.add(new Task("E", 2000));
+        tasks.add(new Task("F", 3000));
 
      //   for (int i = 0 ; i < tasks.size() ; i++ ) {
             System.out.println(doTasks(tasks));

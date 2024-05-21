02e46bd6b5c824a7cfbda2fffc9a5447117e7246
Fateme Lashkari <fatemelashkariii84@gamil.com>
2024-05-21 18:08:11 +0330

    debugging thread part, which it has an error

--- src/main/java/sbu/cs/MatrixMultiplication.java
+++ src/main/java/sbu/cs/MatrixMultiplication.java
@@ -24,9 +24,9 @@ public class MatrixMultiplication {
             TODO
                 Perform the calculation and store the final values in tempMatrixProduct
             */
-            multiplyingMatrix(matrix_A , matrix_B);
+            tempMatrixProduct = multiplyingMatrix(matrix_A , matrix_B);
         }
-        public List<List<Integer>> multiplyingMatrix(List<List<Integer>> matrix_A, List<List<Integer>> matrix_B) { //this method will multiply the matrices
+        public List<List<Integer>> multiplyingMatrix(List<List<Integer>> matrix_A, List<List<Integer>> matrix_B) { //this method will multiply that two matrices which we pass it
             int rows1 = matrix_A.size();
             int cols1 = matrix_A.get(0).size();
             int cols2 = matrix_B.get(0).size();
@@ -58,13 +58,13 @@ public class MatrixMultiplication {
         List<List<Integer>> finalMatrix = new ArrayList<>();
         // combining block 1 & block 2 (upper blocks)
         for (int i = 0 ; i < block1.size() ; i++) {
-            List<Integer> row = new ArrayList<>(block1.get(i)); // copying the row of the block1
+            List<Integer> row = new ArrayList<>(block1.get(i));
             row.addAll(block2.get(i));
             finalMatrix.add(row);
         }
         // combining block 3 & block 4 (lower blocks)
         for (int i = 0 ; i < block3.size() ; i++) {
-            List<Integer> row = new ArrayList<>(block3.get(i)); // copying the row of the block1
+            List<Integer> row = new ArrayList<>(block3.get(i));
             row.addAll(block4.get(i));
             finalMatrix.add(row);
         }
@@ -78,12 +78,6 @@ public class MatrixMultiplication {
     */
     public static List<List<Integer>> ParallelizeMatMul(List<List<Integer>> matrix_A, List<List<Integer>> matrix_B)
     {
-        /*
-        TODO
-            Parallelize the matrix multiplication by dividing tasks between 4 threads.
-            Each thread should calculate one block of the final matrix product. Each block should be a quarter of the final matrix.
-            Combine the 4 resulting blocks to create the final matrix product and return it.
-         */
         List<List<Integer>> temp1 = new ArrayList<>();
         List<List<Integer>> temp2 = new ArrayList<>();
         List<List<Integer>> temp3 = new ArrayList<>();
@@ -107,7 +101,6 @@ public class MatrixMultiplication {
             temp3.add(matrix_A.get(i));
         }
 
-
         for (int i = 0; i < matrix_B.size(); i++) { // the right part of the matrix ---> thread 4
             List<Integer> row = new ArrayList<>();
             for (int j = matrix_B.get(0).size()/2 ; j < matrix_B.get(0).size() ; j++) {
@@ -115,11 +108,10 @@ public class MatrixMultiplication {
             }
             temp4.add(row);
         }
+
         // ----------------------------------------------for separating the matrix---------------------------------------------------
 
         List<BlockMultiplier> listOfBlocks = new ArrayList<>();
-        List<Thread> listOfThreads = new ArrayList<>();
-
 
         BlockMultiplier blockMultiplier1 = new BlockMultiplier(temp1 , temp2); //up & left
         listOfBlocks.add(blockMultiplier1);
@@ -132,24 +124,40 @@ public class MatrixMultiplication {
 
         BlockMultiplier blockMultiplier4 = new BlockMultiplier(temp3 , temp4); //down & right
         listOfBlocks.add(blockMultiplier4);
+
         // -------------------------------------------------------------------------------------------------------------------------
+
         // ----------------------------------------------for starting the threads---------------------------------------------------
-        for (int i = 0 ; i < listOfBlocks.size() ; i++) {
-            Thread thread = new Thread(listOfBlocks.get(i));
+
+        List<Thread> listOfThreads = new ArrayList<>();
+        for (BlockMultiplier blockMultiplier : listOfBlocks) {
+            Thread thread = new Thread(blockMultiplier);
             listOfThreads.add(thread);
         }
 
-        for (int i = 0 ; i < listOfThreads.size() ; i++) {
-            listOfThreads.get(i).start();
-
+        for (Thread listOfThread : listOfThreads) {
+            listOfThread.start();
         }
+
+        for (Thread thread : listOfThreads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
         // ----------------------------------------------for starting the threads---------------------------------------------------
         // -------------------------------------------------------------------------------------------------------------------------
         // ---------------------------------- combining the four matrices(blocks) together ------------------------------------------
+
         MatrixMultiplication matrixMultiplication = new MatrixMultiplication();
+
         // ---------------------------------- combining the four matrices(blocks) together ------------------------------------------
         // -------------------------------------------------------------------------------------------------------------------------
-        return matrixMultiplication.khodeMatrix(blockMultiplier1.tempMatrixProduct , blockMultiplier2.tempMatrixProduct , blockMultiplier3.tempMatrixProduct , blockMultiplier4.tempMatrixProduct);
+        List<List<Integer>> matrix = matrixMultiplication.khodeMatrix(blockMultiplier1.tempMatrixProduct ,
+                blockMultiplier2.tempMatrixProduct , blockMultiplier3.tempMatrixProduct , blockMultiplier4.tempMatrixProduct);
+        return matrix;
     }
 
     public static void main(String[] args) {
@@ -161,27 +169,41 @@ public class MatrixMultiplication {
         row1A.add(2);
         row1A.add(3);
         List<Integer> row2A = new ArrayList<>();
-        row1A.add(4);
-        row1A.add(5);
-        row1A.add(6);
+        row2A.add(4);
+        row2A.add(5);
+        row2A.add(6);
         List<Integer> row3A = new ArrayList<>();
-        row1A.add(7);
-        row1A.add(8);
-        row1A.add(9);
+        row3A.add(7);
+        row3A.add(8);
+        row3A.add(9);
         List<Integer> row4A = new ArrayList<>();
-        row1A.add(10);
-        row1A.add(11);
-        row1A.add(12);
+        row4A.add(10);
+        row4A.add(11);
+        row4A.add(12);
+        matrix_A.add(row1A);
+        matrix_A.add(row2A);
+        matrix_A.add(row3A);
+        matrix_A.add(row4A);
         List<Integer> row1B = new ArrayList<>();
-        row1A.add(13);
-        row1A.add(14);
+        row1B.add(13);
+        row1B.add(14);
         List<Integer> row2B = new ArrayList<>();
-        row1A.add(17);
-        row1A.add(16);
+        row2B.add(17);
+        row2B.add(16);
         List<Integer> row3B = new ArrayList<>();
-        row1A.add(17);
-        row1A.add(18);
+        row3B.add(17);
+        row3B.add(18);
+        matrix_B.add(row1B);
+        matrix_B.add(row2B);
+        matrix_B.add(row3B);
         BlockMultiplier blockMultiplier = new BlockMultiplier(matrix_A , matrix_B);
-        blockMultiplier.multiplyingMatrix(matrix_A , matrix_B);
+        List<List<Integer>> result;
+        result = blockMultiplier.multiplyingMatrix(matrix_A , matrix_B);
+        for (List<Integer> innerList : result) {
+            for (Integer element : innerList) {
+                System.out.print(element + " ");
+            }
+            System.out.println();
+        }
     }
 }
